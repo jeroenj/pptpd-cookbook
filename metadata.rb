f994e852ea320864b62b8d9d5e2045039f9fec99
Jeroen Jacobs <git@jeroenj.be>
2015-04-06 12:22:17 +0200

    Bumps version to v1.0.1

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Jeroen Jacobs'
 maintainer_email  'chef@jeroenj.be'
 license           'MIT'
 description       'pptpd cookbook for Bender.'
-version           '1.0.0'
+version           '1.0.1'
 
 depends           'iptables'
 
